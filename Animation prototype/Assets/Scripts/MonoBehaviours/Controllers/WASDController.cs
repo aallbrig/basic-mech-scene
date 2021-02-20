3b4a8550957c82e055c4fb54fd3838589e1752cb
Andrew Allbright <aallbrig@gmail.com>
2021-02-20 16:47:23 -0500

    WASD controls

---
+++ Animation prototype/Assets/Scripts/MonoBehaviours/Controllers/WASDController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WASDController : MonoBehaviour
+{
+    private float _speed = 5.0f;
+    private Transform _transform;
+    private void Start()
+    {
+        _transform = transform;
+    }
+
+    private void Update()
+    {
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+        
+        _transform.Translate(hInput * _speed * Time.deltaTime, 0, vInput * _speed * Time.deltaTime);
+    }
+}
